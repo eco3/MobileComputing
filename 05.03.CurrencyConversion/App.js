a5f228542bed88174c1c2e43fd558f438c4d3c0b
Ekrem <ekrem.emre@posteo.de>
2021-12-05 22:02:24 +0100

    done with 05.03

--- 05.03.CurrencyConversion/App.js
+++ 05.03.CurrencyConversion/App.js
@@ -4,33 +4,121 @@ import {
   StyleSheet,
   View,
   Text,
+  TextInput,
 } from 'react-native';
+
+
+class CurrencyInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.currencyNames = {
+      usd: 'USD',
+      eur: 'Euro'
+    };
+  }
+
+  handleChange(e) {
+    this.props.onAmountChange(e);
+  }
+
+  render() {
+    const amount = this.props.amount;
+    const currency = this.props.currency;
+    return (
+      <View>
+        <Text style={styles.title}>Enter amount in {this.currencyNames[currency]}:</Text>
+        <TextInput
+          style={styles.input}
+          value={amount}
+          onChangeText={this.handleChange} />
+      </View>
+    );
+  }
+}
 
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {amount: '', currency: 'eur'};
+    this.handleEURChange = this.handleEURChange.bind(this);
+    this.handleUSDChange = this.handleUSDChange.bind(this);
+  }
+
+  handleEURChange(amount) {
+    this.setState({currency: 'eur', amount});
+  }
+
+  handleUSDChange(amount) {
+    this.setState({currency: 'usd', amount});
+  }
+
+  toEUR(usd) {
+    return usd * 0.86;
+  }
+  
+  toUSD(eur) {
+    return eur * 1.16;
+  }
+
+  tryConvert(amount, convert) {
+    const input = parseFloat(amount);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const converted = convert(input);
+    return parseFloat(converted).toFixed(2).toString();
+  }
+
+  financeAdvice(amount) {
+    return (amount >= 100) ? "That's a lot." : "Please save some money.";
   }
 
   render() {
-      return (
-        <SafeAreaView style={styles.container}>
-          <View>
-            <Text style={styles.title}>Hello World</Text>
-          </View>
-        </SafeAreaView>
-      );
+    const currency = this.state.currency;
+    const amount = this.state.amount;
+    const eur = currency === 'usd' ? this.tryConvert(amount, this.toEUR) : amount;
+    const usd = currency === 'eur' ? this.tryConvert(amount, this.toUSD) : amount;
+    const adviceAmount = currency === 'usd' ? eur : usd;
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <CurrencyInput
+          currency="eur"
+          amount={eur}
+          onAmountChange={this.handleEURChange} />
+        <CurrencyInput
+          currency="usd"
+          amount={usd}
+          onAmountChange={this.handleUSDChange} />
+        <Text style={styles.advice}>{this.financeAdvice(adviceAmount)}</Text>
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    justifyContent: 'center',
+    padding: 50,
+  },
+  input: {
+    height: 40,
+    marginVertical: 5,
+    marginHorizontal: 10,
+    borderWidth: 1,
+    padding: 10,
   },
   title: {
     color: '#000',
     textAlign: 'left',
-    marginVertical: 12,
+    marginVertical: 5,
+  },
+  advice: {
+    color: '#90e',
+    textAlign: 'center',
+    marginVertical: 10,
   },
 });
